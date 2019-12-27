6d281e094d41536b912478d34f1f560eb07b1f54
Angel Afonso <angelafonso60@gmail.com>
2019-12-27 12:39:08 -0400

    refactored wallet and added signing functions

--- go.mod
+++ go.mod
@@ -10,7 +10,6 @@ require (
 	github.com/ethereum/go-ethereum v1.9.9
 	github.com/miguelmota/go-ethereum-hdwallet v0.0.0-20191219011559-4d9d106de2bd
 	github.com/stretchr/testify v1.4.0
-	github.com/tyler-smith/go-bip39 v1.0.2
 	go.etcd.io/bbolt v1.3.3
 	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915 // indirect
 	golang.org/x/sys v0.0.0-20191224085550-c709ea063b76 // indirect
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,49 +1,77 @@
 package wallet
 
 import (
-	"github.com/ethereum/go-ethereum/accounts"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 )
 
-//Wallet struct
+// Signature struct representation
+type Signature struct {
+	R, S *big.Int
+}
+
+// Wallet struct representation
 type Wallet struct {
-	PublicKey  []byte
-	PrivateKey []byte
-	Address    []byte
-	wallet     *hdwallet.Wallet
-	account    accounts.Account
+	PrivateKey *ecdsa.PrivateKey
+	PublicKey  *ecdsa.PublicKey
+	address    common.Address
 }
 
-//Sign the given data
-func (w *Wallet) Sign(data []byte) []byte {
-	signature, _ := w.wallet.SignText(w.account, data)
-	return signature
+//Sign create ECDSA signature
+func (w *Wallet) Sign(data []byte) (*Signature, error) {
+	r, s, err := ecdsa.Sign(rand.Reader, w.PrivateKey, data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Signature{
+		R: r,
+		S: s,
+	}, nil
 }
 
-// GenerateMnemonic return a new mnemonic string
+//PrivateKeyBytes returns private key in bytes
+func (w *Wallet) PrivateKeyBytes() []byte {
+	return crypto.FromECDSA(w.PrivateKey)
+}
+
+//PublicKeyBytes returns public key in bytes
+func (w *Wallet) PublicKeyBytes() []byte {
+	return crypto.FromECDSAPub(w.PublicKey)
+}
+
+//GenerateMnemonic generate new mnemonic
 func GenerateMnemonic() string {
 	mnemonic, _ := hdwallet.NewMnemonic(256)
 	return mnemonic
 }
 
-//GenerateWallet returns new wallet from given mnemonic
-func GenerateWallet(mnemonic string) *Wallet {
+//GenerateWalletFromMnemonic create new wallet from mnemonic
+func GenerateWalletFromMnemonic(mnemonic string) (*Wallet, error) {
 	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
 	if err != nil {
-		println(err.Error())
+		return nil, err
 	}
 
 	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
 	account, _ := wallet.Derive(path, false)
 
-	privateKey, _ := wallet.PrivateKeyBytes(account)
-	publicKey, _ := wallet.PublicKeyBytes(account)
+	privBytes, _ := wallet.PrivateKeyBytes(account)
+	privateKey, _ := crypto.ToECDSA(privBytes)
 
 	return &Wallet{
-		PublicKey:  publicKey,
 		PrivateKey: privateKey,
-		Address:    account.Address.Bytes(),
-		wallet:     wallet,
-		account:    account,
-	}
+		PublicKey:  &privateKey.PublicKey,
+		address:    crypto.PubkeyToAddress(privateKey.PublicKey),
+	}, nil
+}
+
+//VerifySignature check if the signature is valid
+func VerifySignature(publicKey *ecdsa.PublicKey, data []byte, signature Signature) bool {
+	return ecdsa.Verify(publicKey, data, signature.R, signature.S)
 }
